7fdd2134659f9544099bd49f5689625f10798ccc
SY <seyav29@gmail.com>
2023-07-21 15:16:27 -0400

    9

--- Lesson_17/assignments.js
+++
@@ -1,7 +0,0 @@
-///ASSIGNMENTS, LESSON 16, JAVASCRIPT
-
-//1. Implement "silent" variables declaration using "use strict" directive and fix errors.
-
-//2. Implement "use strict" in functions
-
-//3. Implement a function with duplicate parameters
---
+++ Lesson_18/assignments.js
@@ -0,0 +1,24 @@
+///ASSIGNMENTS, LESSON 18, JAVASCRIPT
+// https://github.com/dmproj/JavaScript-Full-Course-Youtube
+
+//1.
+//create a ternary operator to evalueta if a color is red or blue
+
+const color = "blue"; // You can change the value of 'color' to test different colors
+const res = color === "red" ? "The color is Red" : color === "blue" ? "The color is Blue" : "The color is neither Red nor Blue";
+console.log(res); // Output will be based on the value of 'color'
+
+
+//2.
+//create a ternary nested expression to evalueta different colors
+const condition = "red"; // You can change the value of 'condition' to test different colors
+
+const result =
+  condition === "red" ? "The color is Red" :
+  condition === "blue" ? "The color is Blue" :
+  condition === "green" ? "The color is Green" :
+  "Unknown color";
+
+console.log(result);
+
+
