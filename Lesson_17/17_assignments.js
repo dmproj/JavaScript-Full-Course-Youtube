5ca5aa9cef34045c5c814469a6e812c6f64742fb
SY <seyav29@gmail.com>
2023-07-21 15:32:41 -0400

    13

---
+++ Lesson_17/17_assignments.js
@@ -0,0 +1,28 @@
+///ASSIGNMENTS, LESSON 17, JAVASCRIPT
+// https://github.com/dmproj/JavaScript-Full-Course-Youtube
+
+//1. create an "if" statement which berifies if the variable value is true
+let number = 1;
+if (number) {
+  console.log("it's true");
+}
+
+//2. create an "else if" statement which verifies that a condition is true
+let verifyValue = 17;
+
+if (verifyValue == 17) {
+  console.log("The current value is:" + verifyValue);
+} else if (verifyValue == 18) {
+  console.log("The current value is:" + verifyValue);
+} else if (verifyValue == 19) {
+  console.log("The current value is:" + verifyValue);
+}
+
+//3. create an if, elde-if, else statement which verifies that a condition is true
+let newValue = 22;
+
+if (newValue == 20) {
+  console.log("The value is 20");
+} else if (newValue == 21) {
+  console.log("The value is 21");
+} else {console.log("no match!")} 
---
+++ Lesson_18/1_ternary.js
@@ -0,0 +1,45 @@
+// Conditional / ternary operator IN JAVASCRIPT, LESSON 18
+// https://github.com/dmproj/JavaScript-Full-Course-Youtube
+
+// condition ? expression if True : expression if False
+
+// conditional / ternary operator, similar to if else statement
+const age = 25;
+const result = age >= 18 ? "You are an adult" : "You are a minor";
+// const result = (age >= 18) ? "You are an adult" : "You are a minor";
+console.log(result);
+
+const age1 = 17;
+if (age1 >= 18) {
+  console.log("You are an adult")
+} else console.log("You are a minor")
+
+condition1 ? expression1 : condition2 ? expression2 : condition3 ? expression3 : expression4
+
+const score = 85;
+const res =
+  score >= 90 ? "Excellent" :
+  score >= 80 ? "Good" :
+  score >= 70 ? "Average" :
+  "Needs Improvement";
+
+console.log(res);
+
+// const score = 85;
+// let result;
+
+// if (score >= 90) {
+//   result = "Excellent";
+// } else if (score >= 80) {
+//   result = "Good";
+// } else if (score >= 70) {
+//   result = "Average";
+// } else {
+//   result = "Needs Improvement";
+// }
+
+// console.log(result);
+
+
+
+
